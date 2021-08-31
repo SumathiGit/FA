cc0c49a9d3a790e99f4a5dacbecd28ecc5beefe9
Tae <83117749+SumathiGit@users.noreply.github.com>
2021-08-31 16:08:46 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,16 +1,15 @@
-
 from typing import Optional
 
 from aioredis import Redis, create_redis_pool
 
-
+#Create a RedisCache instance
 class RedisCache:
     
     def __init__(self):
         self.redis_cache: Optional[Redis] = None
         
     async def init_cache(self):
-        self.redis_cache = await create_redis_pool("redis://localhost:6379/0?encoding=utf-8")
+        self.redis_cache = await create_redis_pool("redis://localhost:6379/0?encoding=utf-8") #Connecting to database
 
     async def keys(self, pattern):
         return await self.redis_cache.keys(pattern)
@@ -29,9 +28,14 @@ class RedisCache:
 
     async def hvals(self, key):
         return await self.redis_cache.hvals(key)
-    
+
     async def hget(self, key, field):
         return await self.redis_cache.hget(key , field)
+    
+    async def smembers(self, key):
+        result = self.redis_cache.smembers(key)
+        return await result
+
     
     async def close(self):
         self.redis_cache.close()
